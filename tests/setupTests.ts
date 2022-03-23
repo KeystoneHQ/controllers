47b507d3c6efc1ae4d507ef292d0e03840269753
Soralit <soralitria@gmail.com>
2022-03-23 17:26:31 +0800

    refactor unit tests

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,1 +1,9 @@
 import 'isomorphic-fetch';
+
+// some libraries (e.g. uuid) use defineProperty to export, so jest.mock and sinon.stub won't work.
+// add this to make the properties configurable and we can mock the library.
+const _defineProperty = Object.defineProperty;
+Object.defineProperty = function (obj, prop, descriptor) {
+  descriptor.configurable = true;
+  return _defineProperty(obj, prop, descriptor);
+};
